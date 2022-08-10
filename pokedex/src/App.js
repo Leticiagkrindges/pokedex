d06469b5dfe031e01a426c4fecc0085558b79562
lgk_ <lgk@neo.certi.org.br>
2022-08-10 18:01:47 -0300

    wip conecting to api

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -2,36 +2,56 @@ import React, { useEffect, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 import api from './service/api';
+import axios from 'axios';
 
 
 function createData(name, url) {
   return { name, url };
 };
 
+function createList(url){
+  return{ url};
+};
+
 
 function App() {
-  const [pokemon, setPokemon] = useState();
-  const [pokemon1, setPokemon1] = useState();
+  const [pokemon, setPokemon] = useState([]);
+  const [pokemon1, setPokemon1] = useState([]);
   const [results] = useState([]);
+  const [pokeurl, setPokeurl] = useState();
+  const [id, setId] = useState([]);
+
 
   useEffect(()=>{
-  api
+    api
     .get("https://pokeapi.co/api/v2/pokemon/")
     .then((response)=>{
       setPokemon(response.data.results);
       console.log(response.data.results);
       let temp_pokemons = [];
       response.data.results.forEach(pokemon => {
-      temp_pokemons.push(createData(pokemon.name, pokemon.url))
+       temp_pokemons.push(createData(pokemon.name, pokemon.url))
+      });
+      let pokeurl = [];
+      response.data.results.forEach(pokemon => {
+        pokeurl.push(createList(pokemon.url))
       });
     setPokeball(temp_pokemons);
+    setPokeurl(pokeurl);
     console.log(Pokeball);
+    console.log(pokeurl);
+    let id = Pokeball.document.getElementeById();
+    setId(id);
+    console.log(id);
+    //console.log(Pokeball.length);  
     })
-  },[])
+  },[]);
+
+  // let id = Pokeball.document.getElementeById(id);
+  // console.log(id);
 
   useEffect(()=>{
-    api
-    .get("https://pokeapi.co/api/v2/pokemon/1/")
+    api.get("https://pokeapi.co/api/v2/pokemon/1")
     .then((response)=>{
       setPokemon1(response.data);
       console.log(response.data);
@@ -58,9 +78,10 @@ function App() {
           {Pokeball.map((pokemon, id)=>{
               return [
               <div key={id}>
-                
-                <h1> name: {pokemon.name}</h1>
-                <h2>url: {pokemon.url} </h2>
+                <a>
+                  <h1>name: {pokemon.name}</h1>
+                  <h2>url: {pokemon.url} </h2>
+                </a>
               </div>
               ]
             })} 
